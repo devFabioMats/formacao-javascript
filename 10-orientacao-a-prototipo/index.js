dadfa64fa5da3d98dc12647f7261caa4205775ec
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-10-04 18:26:03 -0300

    Update index.js

--- 10-orientacao-a-prototipo/index.js
+++ 10-orientacao-a-prototipo/index.js
@@ -1,21 +1,10 @@
-// nao ta errado, mas o de baixo é mais moderno
-function Pessoa(nome, idade) {  // funcao que começa maiuscula é uma função construtora
-    this.nome = nome;
-    this.idade = idade;
+const pessoa = {
+    genero: 'masculino'
 }
 
-Pessoa.prototype.falar = function() {       // alterei o protitipo de pessoa e adicionei um metodo falar
-    console.log(`${this.nome} está falando`);
-}
+const fabio = Object.create(pessoa)
 
-// usar assim
-class Pessoa {
-    constructor(nome, idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
 
-    falar() {
-        console.log(`${this.nome} está falando`);
-    }
-}
+fabio.nome = 'fabio';
+
+console.log(fabio.genero) // 20
